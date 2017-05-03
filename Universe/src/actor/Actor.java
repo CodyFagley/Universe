c94ab06b99660e42c00562257374714890f9a830
Cody Fagley <cody.fagley@gmail.com>
2017-05-03 15:48:38 -0600

    Added Actor Class / Generic Person Class

---
+++ Universe/src/actor/Actor.java
@@ -0,0 +1,14 @@
+/**
+ *  Actor.java
+ *  Authored by Cody Fagley
+ *  May 3, 2017
+ */
+
+package actor;
+
+public class Actor 
+{
+    //  Location Variables
+    int x;  //  x is the number of tiles right from (0, 0)
+    int y;  //  y is the number of tiles below (0, 0)
+}
