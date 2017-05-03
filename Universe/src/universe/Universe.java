8693955fb00bc32b27fb554bffc39b20b629c14b
Cody Fagley <cody.fagley@gmail.com>
2017-05-03 15:01:12 -0600

    Made Basic Frame with ESC close interface

---
+++ Universe/src/universe/Universe.java
@@ -0,0 +1,21 @@
+/*
+ *  Universe.java
+ *  Authored by Cody Fagley
+ *  May 03, 2017
+ */
+package universe;
+
+public class Universe 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        //  Create a new instance of the Application, Universe
+        App app = new App();
+        app.setVisible(true);
+    }
+  
+}
